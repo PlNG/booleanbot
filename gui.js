24f75e83ebaa0bd3110afaf1df2292e3537b6659
PlNG <tempeml@hotmail.com>
2013-11-25 12:04:35 -0500

    Dumbing down syntax for JSLint
    JSLint treats valid syntax: "for (var" as a parsing error rather than a warning.

--- gui.js
+++ gui.js
@@ -31,7 +31,8 @@ $(document).ready(function () {
         } catch (e) {
             if (e instanceof InvalidTokenException) {
                 var message = "Syntax Error: ";
-                for (var i = 0; i < input.length; i++) {
+                var i;
+                for (i = 0; i < input.length; i++) {
                     if (i + 1 == e.position) {
                         message += "<strong class='error'>" + input[i] + "</strong>";
                     } else {
@@ -44,7 +45,8 @@ $(document).ready(function () {
             } else {
                 if (e instanceof MissingTokenException) {
                     var message = "Missing Token: ";
-                    for (var i = 0; i < input.length; i++) {
+                    var i;
+                    for (i = 0; i < input.length; i++) {
                         if (i + 1 == e.position) {
                             message += "<strong class='error'>" + input[i] + "</strong>";
                         } else {
@@ -76,7 +78,8 @@ $(document).ready(function () {
         }
         var terms = input.split(",");
         var output = [];
-        for (var i = 0; i < terms.length; i++) {
+        var i;
+        for (i = 0; i < terms.length; i++) {
             if ($.trim(terms[i]) == "") {
                 continue;
             }
@@ -86,7 +89,8 @@ $(document).ready(function () {
             if (sub_terms.length == 2) { // it's a range
                 var start = parseInt($.trim(sub_terms[0]), 10);
                 var end = parseInt($.trim(sub_terms[1]), 10);
-                for (var j = start; j <= end; j++) {
+                var j;
+                for (j = start; j <= end; j++) {
                     output.push(j);
                 }
             } else { // not a range, just add the term
@@ -111,7 +115,8 @@ $(document).ready(function () {
         var input = $("#truth-table-panel-one-input").val();
         var vars_input = input.split(",");
         var vars = [];
-        for (var i = 0; i < vars_input.length; i++) {
+        var i;
+        for (i = 0; i < vars_input.length; i++) {
             var v = $.trim(vars_input[i]);
             if (v == "") {
                 continue;
@@ -124,14 +129,17 @@ $(document).ready(function () {
         // add header
         var table_header = $("<tr>");
         table_header.append("<th>#</th>");
-        for (var i = 0; i < vars.length; i++) {
+        var i;
+        for (i = 0; i < vars.length; i++) {
             table_header.append("<th>" + vars[i] + "</th>");
         }
         table_header.append("<th>Out</th>");
         $("#truth-table-panel-two table thead").append(table_header);
         // add table body
         var table_body = [];
-        for (var iter = new TruthTableIterator(vars), n = 0; iter.hasNext(); n++) {
+        var iter = new TruthTableIterator(vars);
+        var n;
+        for (n = 0; iter.hasNext(); n++) {
             if (n % 2 == 0) {
                 table_body.push("<tr class='toggle-me'>");
             } else {
@@ -139,7 +147,8 @@ $(document).ready(function () {
             }
             var symbol_values = iter.next();
             table_body.push("<th>" + n + "</th>");
-            for (var i = 0; i < vars.length; i++) {
+            var i;
+            for (i = 0; i < vars.length; i++) {
                 table_body.push("<td>");
                 table_body.push(symbol_values[vars[i]]);
                 table_body.push("</td>");
@@ -175,12 +184,14 @@ $(document).ready(function () {
         $(".panel").hide();
         var $ones = $("#truth-table-panel-two-body .one");
         var ones = [];
-        for (var i = 0; i < $ones.length; i++) {
+        var i;
+        for (i = 0; i < $ones.length; i++) {
             ones.push(parseInt($($ones[i]).attr("id").substr(2), 10));
         }
         var $dont_cares = $("#truth-table-panel-two-body .dont-care");
         var dont_cares = [];
-        for (var i = 0; i < $dont_cares.length; i++) {
+        var i;
+        for (i = 0; i < $dont_cares.length; i++) {
             dont_cares.push(parseInt($($dont_cares[i]).attr("id").substr(2), 10));
         }
         var min_terms = MinTerms.fromArray(ones, dont_cares);
@@ -197,14 +208,17 @@ function BooleanFunctionOut(f, vars) {
         // add header
         var table_header = $("<tr>");
         table_header.append("<th>#</th>");
-        for (var i = 0; i < vars.length; i++) {
+        var i;
+        for (i = 0; i < vars.length; i++) {
             table_header.append("<th>" + vars[i] + "</th>");
         }
         table_header.append("<th>Out</th>");
         $("#truth-table-output table thead").append(table_header);
         // add table body
         var table_body = [];
-        for (var iter = new TruthTableIterator(vars), n = 0; iter.hasNext(); n++) {
+        var iter = new TruthTableIterator(vars);
+        var n;
+        for (n = 0; iter.hasNext(); n++) {
             if (n % 2 == 0) {
                 table_body.push("<tr>");
             } else {
@@ -212,7 +226,8 @@ function BooleanFunctionOut(f, vars) {
             }
             var symbol_values = iter.next();
             table_body.push("<th>" + n + "</th>");
-            for (var i = 0; i < vars.length; i++) {
+            var i;
+            for (i = 0; i < vars.length; i++) {
                 table_body.push("<td>");
                 table_body.push(symbol_values[vars[i]]);
                 table_body.push("</td>");
@@ -232,7 +247,8 @@ function BooleanFunctionOut(f, vars) {
         var solns = SumOfProducts.reduce(sum_of_prods);
         var pretty = SumOfProducts.toSymbols(solns, prime_imps, vars);
         $("#expressions-output-body tr").remove();
-        for (var i = 0; i < pretty.length; i++) {
+        var i;
+        for (i = 0; i < pretty.length; i++) {
             $("#expressions-output-body").append("<tr><td>" + pretty[i] + "</td></tr>");
         }
         f = new BooleanFunction(MinTerms.fromExpression(pretty[0]));
@@ -240,7 +256,8 @@ function BooleanFunctionOut(f, vars) {
     this.buildMinTermList = function () {
         var terms = f.getMinTerms();
         var mins = [];
-        for (var i = 0; i < terms.length; i++) {
+        var i;
+        for (i = 0; i < terms.length; i++) {
             mins.push(terms[i].covers[0]);
         }
         mins.sort(function (a, b) {
