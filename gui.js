2b6168e20bda89ff2061c951f2237b73499a3df0
PlNG <tempeml@hotmail.com>
2013-11-25 13:40:37 -0500

    Satisfy JSLint
    JSLint Edition 2013-11-23
    /*jslint browser: true, bitwise: true, plusplus: true, white: true, maxerr: 9999 */

--- gui.js
+++ gui.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    "use strict";
     $(".panel").hide();
     $("#home-panel").show();
     $("#back").click(function () {
@@ -25,15 +26,14 @@ $(document).ready(function () {
         $("#min-terms-panel-input").focus();
     });
     $("#expression-panel-submit").click(function () {
-        var input = $("#expression-panel-input").val();
+        var min_terms, message, i, f, vars, drawer, input = $("#expression-panel-input").val();
         try {
-            var min_terms = MinTerms.fromExpression(input);
+            min_terms = MinTerms.fromExpression(input);
         } catch (e) {
             if (e instanceof InvalidTokenException) {
-                var message = "Syntax Error: ";
-                var i;
+                message = "Syntax Error: ";
                 for (i = 0; i < input.length; i++) {
-                    if (i + 1 == e.position) {
+                    if (i + 1 === e.position) {
                         message += "<strong class='error'>" + input[i] + "</strong>";
                     } else {
                         message += input[i];
@@ -42,112 +42,102 @@ $(document).ready(function () {
                 $("#expression-panel-input-message").html(message);
                 $("#expression-panel-input-message").removeClass("hide");
                 return;
-            } else {
-                if (e instanceof MissingTokenException) {
-                    var message = "Missing Token: ";
-                    var i;
-                    for (i = 0; i < input.length; i++) {
-                        if (i + 1 == e.position) {
-                            message += "<strong class='error'>" + input[i] + "</strong>";
-                        } else {
-                            message += input[i];
-                        }
-                    }
-                    $("#expression-panel-input-message").html(message);
-                    $("#expression-panel-input-message").removeClass("hide");
-                    return;
-                } else {
-                    $("#expression-panel-input-message").html("Invalid Expression!");
-                    $("#expression-panel-input-message").removeClass("hide");
-                    return;
-                }
             }
+            if (e instanceof MissingTokenException) {
+                message = "Missing Token: ";
+                for (i = 0; i < input.length; i++) {
+                    if (i + 1 === e.position) {
+                        message += "<strong class='error'>" + input[i] + "</strong>";
+                    } else {
+                        message += input[i];
+                    }
+                }
+                $("#expression-panel-input-message").html(message);
+                $("#expression-panel-input-message").removeClass("hide");
+                return;
+            }
+            $("#expression-panel-input-message").html("Invalid Expression!");
+            $("#expression-panel-input-message").removeClass("hide");
+            return;
         }
         $("#expression-panel-input-message").addClass("hide");
         $(".panel").hide();
-        var f = new BooleanFunction(min_terms);
+        f = new BooleanFunction(min_terms);
         // draw the output using the variables in the expression
-        var vars = SumOfProducts.removeDuplicates(input.replace(/[^a-zA-Z]/g, "").split(""));
+        vars = SumOfProducts.removeDuplicates(input.replace(/[^a-zA-Z]/g, "").split(""));
         vars.sort();
-        var drawer = new BooleanFunctionOut(f, vars);
+        drawer = new BooleanFunctionOut(f, vars);
         drawer.render();
     });
     var termInputStringToArray = function (input) {
-        if ($.trim(input) == "") {
+        if ($.trim(input) === "") {
             return [];
         }
-        var terms = input.split(",");
-        var output = [];
-        var i;
+        var i, sub_terms, start, end, j, terms = input.split(","),
+            output = [];
         for (i = 0; i < terms.length; i++) {
-            if ($.trim(terms[i]) == "") {
-                continue;
-            }
-            // min terms can be input as a range (e.g. 2-6)
-            // if they are, we need to add all the terms in the range
-            var sub_terms = terms[i].split("-");
-            if (sub_terms.length == 2) { // it's a range
-                var start = parseInt($.trim(sub_terms[0]), 10);
-                var end = parseInt($.trim(sub_terms[1]), 10);
-                var j;
-                for (j = start; j <= end; j++) {
-                    output.push(j);
+            if ($.trim(terms[i]) !== "") {
+                // min terms can be input as a range (e.g. 2-6)
+                // if they are, we need to add all the terms in the range
+                sub_terms = terms[i].split("-");
+                if (sub_terms.length === 2) { // it's a range
+                    start = parseInt($.trim(sub_terms[0]), 10);
+                    end = parseInt($.trim(sub_terms[1]), 10);
+                    for (j = start; j <= end; j++) {
+                        output.push(j);
+                    }
+                } else { // not a range, just add the term
+                    output.push(parseInt($.trim(terms[i]), 10));
                 }
-            } else { // not a range, just add the term
-                output.push(parseInt($.trim(terms[i]), 10));
             }
         }
         return output;
     };
     $("#min-terms-panel-submit").click(function () {
         $(".panel").hide();
-        var input = $("#min-terms-panel-input").val();
-        var min_terms = termInputStringToArray(input);
-        var input = $("#min-terms-panel-input-two").val();
-        var dont_cares = termInputStringToArray(input);
-        var min_terms = MinTerms.fromArray(min_terms, dont_cares);
-        var f = new BooleanFunction(min_terms);
-        var drawer = new BooleanFunctionOut(f, "abcdefghijklmnopqrstuvwxyz".split("").slice(0, f.getNumberOfVars()));
+        var dont_cares, f, alpha, drawer, input = $("#min-terms-panel-input").val(),
+            min_terms = termInputStringToArray(input);
+        input = $("#min-terms-panel-input-two").val();
+        dont_cares = termInputStringToArray(input);
+        min_terms = MinTerms.fromArray(min_terms, dont_cares);
+        f = new BooleanFunction(min_terms);
+        alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+        drawer = new BooleanFunctionOut(f, alpha.slice(0, f.getNumberOfVars()));
         drawer.render();
     });
     $("#truth-table-panel-one-submit").click(function () {
         $(".panel").hide();
-        var input = $("#truth-table-panel-one-input").val();
-        var vars_input = input.split(",");
-        var vars = [];
-        var i;
+        var i, v, iter, n, symbol_values, input = $("#truth-table-panel-one-input").val(),
+            vars_input = input.split(","),
+            table_header = $("<tr>"),
+            table_body = [],
+            vars = [];
         for (i = 0; i < vars_input.length; i++) {
-            var v = $.trim(vars_input[i]);
-            if (v == "") {
-                continue;
+            v = $.trim(vars_input[i]);
+            if (v !== "") {
+                vars.push(v);
             }
-            vars.push(v);
         }
         // remove old truth table
         $("#truth-table-panel-two table thead tr").remove();
         $("#truth-table-panel-two table tbody tr").remove();
         // add header
-        var table_header = $("<tr>");
         table_header.append("<th>#</th>");
-        var i;
         for (i = 0; i < vars.length; i++) {
             table_header.append("<th>" + vars[i] + "</th>");
         }
         table_header.append("<th>Out</th>");
         $("#truth-table-panel-two table thead").append(table_header);
         // add table body
-        var table_body = [];
-        var iter = new TruthTableIterator(vars);
-        var n;
+        iter = new TruthTableIterator(vars);
         for (n = 0; iter.hasNext(); n++) {
-            if (n % 2 == 0) {
+            if (n % 2 === 0) {
                 table_body.push("<tr class='toggle-me'>");
             } else {
                 table_body.push("<tr class='odd toggle-me'>");
             }
-            var symbol_values = iter.next();
+            symbol_values = iter.next();
             table_body.push("<th>" + n + "</th>");
-            var i;
             for (i = 0; i < vars.length; i++) {
                 table_body.push("<td>");
                 table_body.push(symbol_values[vars[i]]);
@@ -159,14 +149,14 @@ $(document).ready(function () {
         $("#truth-table-panel-two-body").html(table_body.join(""));
         // handle the clicks
         $(".toggle-me").live("mousedown", function () {
-            var $el = $(this).children("td:last");
-            var val = $el.text();
-            if (val == "0") {
+            var $el = $(this).children("td:last"),
+                val = $el.text();
+            if (val === "0") {
                 $el.text("1");
                 $el.addClass("one");
                 $el.removeClass("dont-care");
             } else {
-                if (val == "1") {
+                if (val === "1") {
                     $el.text("X");
                     $el.addClass("dont-care");
                     $el.removeClass("one");
@@ -182,51 +172,48 @@ $(document).ready(function () {
     });
     $("#truth-table-panel-two-submit").click(function () {
         $(".panel").hide();
-        var $ones = $("#truth-table-panel-two-body .one");
-        var ones = [];
-        var i;
+        var i, min_terms, f, drawer, alpha = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"],
+            ones = [],
+            $ones = $("#truth-table-panel-two-body .one"),
+            dont_cares = [],
+            $dont_cares = $("#truth-table-panel-two-body .dont-care");
         for (i = 0; i < $ones.length; i++) {
             ones.push(parseInt($($ones[i]).attr("id").substr(2), 10));
         }
-        var $dont_cares = $("#truth-table-panel-two-body .dont-care");
-        var dont_cares = [];
-        var i;
         for (i = 0; i < $dont_cares.length; i++) {
             dont_cares.push(parseInt($($dont_cares[i]).attr("id").substr(2), 10));
         }
-        var min_terms = MinTerms.fromArray(ones, dont_cares);
-        var f = new BooleanFunction(min_terms);
-        var drawer = new BooleanFunctionOut(f, "abcdefghijklmnopqrstuvwxyz".split(""));
+        min_terms = MinTerms.fromArray(ones, dont_cares);
+        f = new BooleanFunction(min_terms);
+        drawer = new BooleanFunctionOut(f, alpha);
         drawer.render();
     });
 });
 function BooleanFunctionOut(f, vars) {
+    "use strict";
     this.buildTruthTable = function () {
         // remove old truth table
         $("#truth-table-output table thead tr").remove();
         $("#truth-table-output table tbody tr").remove();
         // add header
-        var table_header = $("<tr>");
+        var i, iter, n, symbol_values, table_header = $("<tr>"),
+            table_body = [];
         table_header.append("<th>#</th>");
-        var i;
         for (i = 0; i < vars.length; i++) {
             table_header.append("<th>" + vars[i] + "</th>");
         }
         table_header.append("<th>Out</th>");
         $("#truth-table-output table thead").append(table_header);
         // add table body
-        var table_body = [];
-        var iter = new TruthTableIterator(vars);
-        var n;
+        iter = new TruthTableIterator(vars);
         for (n = 0; iter.hasNext(); n++) {
-            if (n % 2 == 0) {
+            if (n % 2 === 0) {
                 table_body.push("<tr>");
             } else {
                 table_body.push("<tr class='odd'>");
             }
-            var symbol_values = iter.next();
+            symbol_values = iter.next();
             table_body.push("<th>" + n + "</th>");
-            var i;
             for (i = 0; i < vars.length; i++) {
                 table_body.push("<td>");
                 table_body.push(symbol_values[vars[i]]);
@@ -241,22 +228,20 @@ function BooleanFunctionOut(f, vars) {
         $("#output").show();
     };
     this.buildExpressionList = function () {
-        var prime_imps = f.findPrimeImplicants();
-        var table = PrimeImplicantTable.build(f.getMinTerms(), prime_imps);
-        var sum_of_prods = SumOfProducts.fromTable(table);
-        var solns = SumOfProducts.reduce(sum_of_prods);
-        var pretty = SumOfProducts.toSymbols(solns, prime_imps, vars);
+        var i, prime_imps = f.findPrimeImplicants(),
+            table = PrimeImplicantTable.build(f.getMinTerms(), prime_imps),
+            sum_of_prods = SumOfProducts.fromTable(table),
+            solns = SumOfProducts.reduce(sum_of_prods),
+            pretty = SumOfProducts.toSymbols(solns, prime_imps, vars);
         $("#expressions-output-body tr").remove();
-        var i;
         for (i = 0; i < pretty.length; i++) {
             $("#expressions-output-body").append("<tr><td>" + pretty[i] + "</td></tr>");
         }
         f = new BooleanFunction(MinTerms.fromExpression(pretty[0]));
     };
     this.buildMinTermList = function () {
-        var terms = f.getMinTerms();
-        var mins = [];
-        var i;
+        var i, terms = f.getMinTerms(),
+            mins = [];
         for (i = 0; i < terms.length; i++) {
             mins.push(terms[i].covers[0]);
         }
