7714cd1a9db27d40d0f589bd68304e8bc9562309
PlNG <tempeml@hotmail.com>
2013-11-25 13:40:46 -0500

    Satisfy JSLint
    JSLint Edition 2013-11-23
    /*jslint browser: true, bitwise: true, plusplus: true, white: true, maxerr: 9999 */

--- parser.js
+++ parser.js
@@ -4,8 +4,9 @@ bool_term -> bool_factor {^ bool_factor}
 bool_factor -> bool_atom {bool_atom}
 bool_atom -> bool_atom' | (bool) | var
 */
-Token = {
+var Token = {
     isWhitespace: function (c) {
+        "use strict";
         switch (c) {
         case " ":
         case "\n":
@@ -19,9 +20,11 @@ Token = {
         return false;
     },
     isVariable: function (c) {
-        return c >= "A" && c <= "Z" || c >= "a" && c <= "z";
+        "use strict";
+        return ((c >= "A" && c <= "Z") || (c >= "a" && c <= "z"));
     },
     isOperator: function (c) {
+        "use strict";
         switch (c) {
         case "+":
         case "^":
@@ -32,92 +35,102 @@ Token = {
         }
         return false;
     }
-};
+}, bool, boolTerm, boolFactor, boolAtom; // Because of circular dependencies, it was necessary to convert these to assignment form in order to satisfy JSLint.
 function AndExpression(left, right) {
-    this.eval = function (symbol_values) {
-        return left.eval(symbol_values) && right.eval(symbol_values);
+    "use strict";
+    this.evaluate = function (symbol_values) {
+        return left.evaluate(symbol_values) && right.evaluate(symbol_values);
     };
 }
 function XorExpression(left, right) {
-    this.eval = function (symbol_values) {
-        p = left.eval(symbol_values);
-        q = right.eval(symbol_values);
-        return p && !q || !p && q;
+    "use strict";
+    this.evaluate = function (symbol_values) {
+        var p = left.evaluate(symbol_values),
+            q = right.evaluate(symbol_values);
+        return ((p && !q) || (!p && q));
     };
 }
 function OrExpression(left, right) {
-    this.eval = function (symbol_values) {
-        return left.eval(symbol_values) || right.eval(symbol_values);
+    "use strict";
+    this.evaluate = function (symbol_values) {
+        return left.evaluate(symbol_values) || right.evaluate(symbol_values);
     };
 }
 function VariableExpression(symbol) {
-    this.eval = function (symbol_values) {
+    "use strict";
+    this.evaluate = function (symbol_values) {
         return !!symbol_values[symbol];
     };
 }
 function NotExpression(unary) {
-    this.eval = function (symbol_values) {
-        return !unary.eval(symbol_values);
+    "use strict";
+    this.evaluate = function (symbol_values) {
+        return !unary.evaluate(symbol_values);
     };
 }
-function Bool(lexer) {
-    var e = BoolTerm(lexer);
-    while (1) {
-        if (lexer.token() == "+") {
+bool = function (lexer) {
+    "use strict";
+    var e = boolTerm(lexer);
+    while (true) {
+        if (lexer.token() === "+") {
             lexer.match("+");
-            e = new OrExpression(e, BoolTerm(lexer));
+            e = new OrExpression(e, boolTerm(lexer));
         } else {
             break;
         }
     }
     return e;
-}
-function BoolTerm(lexer) {
-    var e = BoolFactor(lexer);
-    while (1) {
-        if (lexer.token() == "^") {
+};
+boolTerm = function (lexer) {
+    "use strict";
+    var e = boolFactor(lexer);
+    while (true) {
+        if (lexer.token() === "^") {
             lexer.match("^");
-            e = new XorExpression(e, BoolFactor(lexer));
+            e = new XorExpression(e, boolFactor(lexer));
         } else {
             break;
         }
     }
     return e;
-}
-function BoolFactor(lexer) {
-    var e = BoolAtom(lexer);
-    while (1) {
+};
+boolFactor = function (lexer) {
+    "use strict";
+    var e = boolAtom(lexer);
+    while (true) {
         // ANDs can ride up against another variable or a (
-        if (Token.isVariable(lexer.token()) || lexer.token() == "(") {
-            e = new AndExpression(e, BoolAtom(lexer));
+        if (Token.isVariable(lexer.token()) || lexer.token() === "(") {
+            e = new AndExpression(e, boolAtom(lexer));
         } else {
             break;
         }
     }
     return e;
-}
-function BoolAtom(lexer) {
+};
+boolAtom = function (lexer) {
+    "use strict";
     var e = null;
     if (Token.isVariable(lexer.token())) {
         e = new VariableExpression(lexer.token());
         lexer.match(lexer.token());
     } else {
-        if (lexer.token() == "(") {
+        if (lexer.token() === "(") {
             lexer.match("(");
-            e = Bool(lexer);
+            e = bool(lexer);
             lexer.match(")");
         } else {
             lexer.match("  "); // won't match anything, throws missing token exception
         }
     }
     // look for negative;
-    if (lexer.token() == "'") {
+    if (lexer.token() === "'") {
         e = new NotExpression(e);
         lexer.match("'");
     }
     return e;
-}
+};
 function InvalidTokenException(tok, position) {
+    "use strict";
     this.position = position;
     this.tok = tok;
     this.toString = function () {
@@ -125,6 +138,7 @@ function InvalidTokenException(tok, position) {
     };
 }
 function MissingTokenException(given, expected, position) {
+    "use strict";
     this.position = position;
     this.given = given;
     this.expected = expected;
@@ -133,29 +147,29 @@ function MissingTokenException(given, expected, position) {
     };
 }
 function InvalidExpressionException() {
+    "use strict";
     this.toString = function () {
         return "Invalid Expression";
     };
 }
 function BooleanExpressionLexer(expr) {
-    var index = 0;
-    var tokens = [];
-    var parse = function () {
-        // find all the tokens and push them on the stack
-        var i;
-        for (i = 0; i < expr.length; i++) {
-            var tok = expr[i];
-            if (Token.isWhitespace(tok)) {
-                continue;
-            } else {
-                if (Token.isVariable(tok) || Token.isOperator(tok)) {
-                    tokens.push(tok);
-                } else {
-                    throw new InvalidTokenException(tok, i + 1);
+    "use strict";
+    var index = 0,
+        tokens = [],
+        parse = function () {
+            // find all the tokens and push them on the stack
+            var i, tok;
+            for (i = 0; i < expr.length; i++) {
+                tok = expr[i];
+                if (!Token.isWhitespace(tok)) {
+                    if (Token.isVariable(tok) || Token.isOperator(tok)) {
+                        tokens.push(tok);
+                    } else {
+                        throw new InvalidTokenException(tok, i + 1);
+                    }
                 }
             }
-        }
-    };
+        };
     // get the next token on the stack
     this.nextToken = function () {
         return tokens[index++];
@@ -166,7 +180,7 @@ function BooleanExpressionLexer(expr) {
     };
     // verify the current token matches the specified token, and move to the next token
     this.match = function (c) {
-        if (c != this.token()) {
+        if (c !== this.token()) {
             throw new MissingTokenException(this.token(), c, index);
         }
         this.nextToken();
@@ -177,18 +191,18 @@ function BooleanExpressionLexer(expr) {
     // get a sorted list of all the variables in the expression
     this.variables = function () {
         // use a hash table to eliminate duplicate vars
-        var vars = {};
-        var i;
+        var i, k, vars = {},
+            vars_array = [];
         for (i = 0; i < tokens.length; i++) {
             if (Token.isVariable(tokens[i])) {
                 vars[tokens[i]] = tokens[i];
             }
         }
         // now load the variables into an array
-        var vars_array = [];
-        var k;
         for (k in vars) {
-            vars_array.push(vars[k]);
+            if (vars.hasOwnProperty(k)) {
+                vars_array.push(vars[k]);
+            }
         }
         vars_array.sort();
         return vars_array;
